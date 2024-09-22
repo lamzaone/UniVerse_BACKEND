4cedd38303c31a137068b43debf3008eebf958f5
lamzaone <denisdenis009@gmail.com>
2024-09-22 22:47:56 +0300

    added broadcastConnections, online/offline status changes

--- main.py
+++ main.py
@@ -155,59 +155,22 @@ class WebSocketManager:
             for connection in self.textroom_connections[room_id]:
                 await connection.send_text(message)
 
-    # async def get_friends_connected_main(self, user_id: int, db: db_dependency) -> List[int]:
-    #     # Query for the user's friends from the database
-    #     db_user = db.query(models.User).filter(models.User.id == user_id).first()
-    #     if not db_user:
-    #         raise HTTPException(status_code=400, detail="User not found")
-        
-    #     # Assuming friendships are stored in a "Friend" model
-    #     friends = db.query(models.Friend).filter(models.Friend.user_id == user_id).all()
-        
-    #     # Extract friend IDs
-    #     friend_ids = [friend.friend_id for friend in friends]
-        
-    #     # Check which friends are connected via WebSocket to the main server
-    #     connected_friends = [
-    #         friend_id for friend_id in friend_ids
-    #         if any(conn.user_id == friend_id for conn in websocket_manager.main_connections)
-    #     ]
-        
-    #     return connected_friends
-    
-    async def get_users_connected_server(self, server_id: int, db: db_dependency) -> List[int]:
-        connected_users = []
-        # Look for all server member ids to see if they are connected to main socket
-        server_members = db.query(models.ServerMember).filter(models.ServerMember.server_id == server_id).all()
-        for server_member in server_members:
-            if any(conn.user_id == server_member.user_id for conn in websocket_manager.main_connections):
-                connected_users.append(server_member.user_id)
-        owner_id = db.query(models.Server).filter(models.Server.id == server_id).first().owner_id
-        if any(conn.user_id == owner_id for conn in websocket_manager.main_connections):
-            connected_users.append(owner_id)
+    async def broadcastConnections(self,update_message:str, user_id: int, servers: List[int], friends: List[int] = None):
+        # Broadcast to friends on the main server
+        if friends:
+            for connection in self.main_connections:
+                if connection.user_id in friends:
+                    await connection.send_text(f"{user_id}: {update_message}")
 
-        return connected_users
-    
-
-    # TODO: FIX THIS
-    async def broadcast_user_update(self, user_id: int, update_message: str, db: db_dependency):
-
-        # friends = await self.get_friends_connected_main(user_id, db=db)
-        
-        # Get all servers the user is part of
-        server_ids = db.query(models.ServerMember).filter(models.ServerMember.user_id == user_id).all()
-        server_ids = [server_id for server_id, in server_ids]
-
-        
-        # # Broadcast to friends on the main server
-        # for friend_id in friends:
-        #     await websocket_manager.broadcast_main(f"User {friend_id}: {update_message}")
-        
         # Broadcast to users in the same servers
-        for server_id in server_ids:
-            await websocket_manager.broadcast_server(server_id, f"User {user_id}: {update_message}")
+        for server_id in servers:
+            if server_id in self.server_connections:
+                for connection in self.server_connections[server_id]:
+                    await connection.send_text(f"{user_id}: {update_message}")
 
-            # Get all server IDs the user is connected to as a member
+
+
+
 
 
 
@@ -725,21 +688,33 @@ def get_categories_and_rooms(server_id: int, db: Session = Depends(get_db)):
 
 
 @app.websocket("/api/ws/main/{user_id}")
-async def websocket_main_endpoint(websocket: WebSocket, user_id: int):
+async def websocket_main_endpoint(websocket: WebSocket, user_id: int, db: db_dependency):
     """Handle WebSocket connections for the main server."""
     websocket.user_id = user_id
-    await websocket_manager.connect_main(websocket)    
+    await websocket_manager.connect_main(websocket)         # Conect to socket
+    await broadcast_status(user_id,"online", db)            # Broadcast status to all servers
     try:
         while True:
             data = await websocket.receive_text()
             # Handle main server messages or updates
             await websocket_manager.broadcast_main(f"Main Server Update for User {user_id}: {data}")
     except WebSocketDisconnect:
-        websocket_manager.disconnect_main(websocket)
-        await websocket_manager.broadcast_main(f"User {user_id} disconnected from the main server")
+        websocket_manager.disconnect_main(websocket)        # Disconnect from socket
+        await broadcast_status(user_id,"offline", db)       # Broadcast status to all servers
     except Exception as e:
         logging.error(f"Error in main server websocket: {e}")
         websocket_manager.disconnect_main(websocket)
+
+async def broadcast_status(user_id,status:str, db: db_dependency):
+    servers = db.query(models.ServerMember).filter(models.ServerMember.user_id == user_id).all()       # Get all servers of user
+    servers_owner = db.query(models.Server).filter(models.Server.owner_id == user_id).all()            # Get all servers owned by user
+    servers = [server.server_id for server in servers]                                                 # Get all server ids from memberships
+    servers.extend([server.id for server in servers_owner])                                            # Extend server ids with owned servers
+
+    #TODO: Add friends
+    friends = None
+    await websocket_manager.broadcastConnections(status, user_id, servers, friends)
+
 
 @app.websocket("/api/ws/server/{server_id}/{user_id}")
 async def websocket_server_endpoint(websocket: WebSocket, server_id: int, user_id: int):
@@ -747,7 +722,6 @@ async def websocket_server_endpoint(websocket: WebSocket, server_id: int, user_i
     
     websocket.user_id = user_id
     await websocket_manager.connect_server(websocket, server_id)
-
     try:
         while True:
             data = await websocket.receive_text()
@@ -768,7 +742,6 @@ async def websocket_textroom_endpoint(websocket: WebSocket, room_id: int, user_i
     try:
         while True:
             data = await websocket.receive_text()
-            # Handle text room messages
             await websocket_manager.broadcast_textroom( room_id, f"Message from User {user_id} in Text Room {room_id}: {data}")
     except WebSocketDisconnect:
         websocket_manager.disconnect_textroom(websocket, room_id)
@@ -858,10 +831,22 @@ async def get_messages(request: MessagesRetrieve, db: db_dependency):
 
     return messages
 
+async def get_users_connected_server(server_id: int, db: db_dependency) -> List[int]:
+    connected_users = []
+    # Look for all server member ids to see if they are connected to main socket
+    server_members = db.query(models.ServerMember).filter(models.ServerMember.server_id == server_id).all()
+    for server_member in server_members:
+        if any(conn.user_id == server_member.user_id for conn in websocket_manager.main_connections):
+            connected_users.append(server_member.user_id)
+    owner_id = db.query(models.Server).filter(models.Server.id == server_id).first().owner_id
+    if any(conn.user_id == owner_id for conn in websocket_manager.main_connections):
+        connected_users.append(owner_id)
+
+    return connected_users
 
 @app.get("/api/server/{server_id}/online", response_model=List[int])
 async def get_online_members(server_id: int, db: db_dependency):
-    connected_users = await websocket_manager.get_users_connected_server(server_id, db=db)
+    connected_users = await get_users_connected_server(server_id, db=db)
     return connected_users
 
 @app.get("/api/server/{server_id}/users", response_model=List[int])
@@ -883,3 +868,4 @@ if __name__ == "__main__":
         "main:app",  # Change "app:app" to the path where your FastAPI instance is created
         host="0.0.0.0"
     )
+
